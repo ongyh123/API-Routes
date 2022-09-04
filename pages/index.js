f8a697c6f599c9c829b28f56c27cd101460b3070
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2022-09-04 20:45:48 +0800

    add a submit handler

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,17 @@ function HomePage() {
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
 
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value; //.value=the value key in by the user
+    const enteredFeedback = feedbackInputRef.current.value;
+  }
+
   return (
     <div>
       <h1>The Home Page</h1>
-      <form>
+      <form onSubmit={submitFormHandler}>
         <div>
           <label htmlFor='email'>Your Email Address</label>
           <input type='email' id='email' ref={emailInputRef} />
