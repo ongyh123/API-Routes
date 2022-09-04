85af148023ed15a458627f8d5630d6cff473d8fc
ongyh123 <105908156+ongyh123@users.noreply.github.com>
2022-09-04 20:41:11 +0800

    add useRef

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
+// dont add JS to this frontend component to talk to the database
+// expose the database in the frontend code, highly insecure, expose to the visitors
+
+import { useRef } from 'react';
+
 function HomePage() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
   return (
     <div>
       <h1>The Home Page</h1>
       <form>
         <div>
           <label htmlFor='email'>Your Email Address</label>
-          <input type='email' id='email' />
+          <input type='email' id='email' ref={emailInputRef} />
         </div>
         <div>
           <label htmlFor='feedback'>Your Feedback</label>
-          <textarea type='feedback' rows='5'></textarea>
+          <textarea type='feedback' rows='5' ref={feedbackInputRef}></textarea>
         </div>
         <button>Send Feedback</button>
       </form>
